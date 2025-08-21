c91c89f5fdb2fd41078bb0684c075bfb764aff06
Akanji Anthony Guseltony <devguselt@gmail.com>
2025-08-21 23:27:20 +0100

    creating re-useable components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import aboutImg from '../assets/images/about-img.jpg';
 import { IoCheckmarkCircleSharp } from "react-icons/io5";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { HeaderSection } from './utilities/HeaderSection';
+import { GradientCircle } from './utilities/GradientCircle';
 
 const About = () => {
 
@@ -11,25 +13,19 @@ const About = () => {
   return (
     <section id='about' className='w-screen relative'>
 
-      <div className="circle-2 w-[250px] h-[250px] rounded-full absolute bg-linear-to-b from-accent to-secondary top-0"></div>
-      <div className="circle-1 w-[250px] h-[250px] rounded-full absolute bg-linear-to-b from-accent to-secondary bottom-0 right-0"></div>
-      <div className="circle-1 w-[250px] h-[250px] rounded-full absolute bg-linear-to-b from-accent to-secondary top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-       
+      <GradientCircle top={'top-0'} left={'left-0'} bottom={'bottom-0'} right={'right-0'} />
     
       <div className='relative w-full h-full z-20 bg-bg/80 backdrop-blur-md shadow-2xl px-[15%] py-20'>
 
-      <div className='w-full flex justify-between items-center'>
-            <div
+        <div className='w-full flex justify-between items-center'>
+          <div
             className="w-[500px] h-[600px] bg-cover bg-center relative rounded-br-6xl rounded-tl-6xl"
             style={styles}>
              
-            </div>
+          </div>
 
-            <div className='w-[50%]'>
-                  <p className='text-xs text-primary font-bold'>Creative design solutions crafted to inspire and engage.</p>
-                  <h1 className="font-header uppercase text-2xl inline-flex font-bold bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-transparent my-2">Elevare Brands</h1>
-                  <p className='text-sm'>Elevare Design is a creative design studio committed to transforming visions into timeless brand experiences.
-                  We specialize in branding, web design, and digital experiences that blend artistry with strategy. Our goal is simple: help businesses stand out, connect with their audience, and grow with purpose.</p>
+          <div className='w-[50%]'>
+            <HeaderSection intro={'Creative design solutions crafted to inspire and engage.'} header={'Elevare Brands'} details={'Elevare Design is a creative design studio committed to transforming visions into timeless brand experiences.We specialize in branding, web design, and digital experiences that blend artistry with strategy. Our goal is simple: help businesses stand out, connect with their audience, and grow with purpose.'}/>
                 <div className='flex gap-4 my-8'>
                 <div className='w-[50%]'>
                   <span className='font-btn text-sm text-secondary font-semibold'>Elevare Mission</span>
@@ -62,7 +58,7 @@ const About = () => {
           <div className='mt-8 flex-between w-fit gap-16'>
             <div className='flex-center gap-4'>
               <p>Let Build Something Together</p>
-               <FaLongArrowAltRight />
+              <FaLongArrowAltRight />
             </div>
             <button className='btn btn-general'>
               <a href="#contact" className="font-btn">Hire Us</a>
